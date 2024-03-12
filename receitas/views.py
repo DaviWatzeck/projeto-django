3d405bda86c16cb8cf2bc5066329d731e83f400a
DaviWatzeck <campustao@gmail.com>
2024-03-12 12:44:40 -0300

    removido contato e sobre.

--- receitas/views.py
+++ receitas/views.py
@@ -1,14 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'receitas/home.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre 3')
-
-
-def contato(request):
-    return HttpResponse('contato 4')
+    return render(request, 'receitas/home.html', context={
+        'name': 'Davi',
+    })
