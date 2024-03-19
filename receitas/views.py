e66af2bf4036346b94b5e1b0329a3c190f01c689
DaviWatzeck <campustao@gmail.com>
2024-03-19 10:44:17 -0300

    mexendo nas configurações do DJANGO e ajustando o site

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def home(request):
     return render(request, 'receitas/pages/home.html', context={
-        'name': 'Davi Watzeck',
+        'receitas': [make_recipe() for _ in range(10)],
+    })
+
+
+def receita(request, id):
+    return render(request, 'receitas/pages/recipe-view.html', context={
+        'receita': make_recipe(),
+        'is_detail_page': True,
     })
